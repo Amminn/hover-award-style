4a5c34de4ba15e9eada663d1e3e74196fc975ec3
Amin <55767303+Amminn@users.noreply.github.com>
2025-05-15 18:03:17 +0100

    feat: small update for hosting

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/hover-award-style/", // replace with your repo name
+  build: {
+    outDir: "dist",
+  },
+});
